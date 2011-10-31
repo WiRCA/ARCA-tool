f8a6c87770b4dfe3906ed857f62ab980fada0f76
ecimon <elaukkane@gmail.com>
2011-10-31 19:43:51 +0200

    Removed references between different databases.

--- db/migrate/model/1.up.create_cause.sql
+++ db/migrate/model/1.up.create_cause.sql
@@ -2,7 +2,7 @@
 CREATE TABLE cause (
   `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255),
-  `creator_id` BIGINT UNSIGNED NOT NULL REFERENCES user(id),
+  `creator_id` BIGINT UNSIGNED NOT NULL,
   `case_id` BIGINT UNSIGNED NOT NULL REFERENCES rcacase(id),
   PRIMARY KEY (`id`)
 );
@@ -18,12 +18,12 @@ CREATE TABLE rcacase (
   `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255),
   `problem_id` BIGINT UNSIGNED NOT NULL REFERENCES cause(id),
-  `owner_id` BIGINT UNSIGNED NOT NULL REFERENCES user(id),
+  `owner_id` BIGINT UNSIGNED NOT NULL,
   PRIMARY KEY (`id`)
 );
 
 CREATE TABLE usercases (
-  `user_id` BIGINT UNSIGNED NOT NULL REFERENCES user(id),
+  `user_id` BIGINT UNSIGNED NOT NULL,
   `case_id` BIGINT UNSIGNED NOT NULL REFERENCES rcacase(id),
   PRIMARY KEY (`userid`, `case_id`)
 );
