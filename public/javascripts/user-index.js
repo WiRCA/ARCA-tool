b6f77e5b7b3cb3d49bf2f1ce29cdf0d762a16e7a
Risto Virtanen <rhjvirta@gmail.com>
2014-01-13 07:03:18 +0200

    Added pagination for RCA case listing

--- public/javascripts/user-index.js
+++ public/javascripts/user-index.js
@@ -99,15 +99,29 @@ $(function() {// element variables
         return false;
     });
 
+    // define pager options
+    var pagerOptions = {
+        // target the pager markup - see the HTML block below
+        container: $("#pager"),
+        // output string - default is '{page}/{totalPages}'; possible variables: {page}, {totalPages}, {startRow}, {endRow} and {totalRows}
+        output: '{startRow} - {endRow} / {filteredRows} ({totalRows})',
+        // if true, the table will remain the same height no matter how many records are displayed. The space is made up by an empty
+        // table row set to a height to compensate; default is false
+        fixedHeight: true,
+        // remove rows from the table to speed up the sort of large tables.
+        // setting this to false, only hides the non-visible rows; needed if you plan to add/remove rows with the pager enabled.
+        removeRows: false
+    };
+
     $("#casesTable").tablesorter({
         // Modified, descending
         sortList:[[1,1]],
         headers: {
             1: { sorter: "shortDate", dateFormat: "ddmmyyyy" },
             2: { sorter: "shortDate", dateFormat: "ddmmyyyy" },
-            3: { sorter: false}
+            4: { sorter: false}
         }
-    });
+    }).tablesorterPager(pagerOptions);
 });
 
 $("a[id^='rcacase-share-']").live("click", function () {
